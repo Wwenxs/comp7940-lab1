8937413665cb38226dac69d5bedf7411ae06d61e
Wwenxs <877510916@qq.com>
2024-03-08 22:52:21 +0800

    change 4 lab6

--- chatbot.py
+++ chatbot.py
@@ -13,7 +13,7 @@ def main():
 #    config = configparser.ConfigParser()
 #    config.read('config.ini')
 #    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
-    updater = Updater(token=(os.version['ACCESS_TOKEN']),use_context=True)
+    updater = Updater(token=(os.environ['ACCESS_TOKEN']),use_context=True)
     dispatcher = updater.dispatcher
     global redis1
     redis1 = redis.Redis(host=(config['REDIS']['HOST']),
